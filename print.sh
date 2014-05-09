4b10a57408a29256bcfc612ba46199123c0e3473
djbartos93 <djbartos93@gmail.com>
2014-05-09 19:53:56 -0400

    stopped the printer from feeding so much after a tweet

--- print.sh
+++ print.sh
@@ -7,11 +7,11 @@
 echo "making some pbm files from the text output file"
 
 #This is the guts of the operation. converiting the text file to a pbm
-convert -size 600x300  -pointsize 40 caption:@/home/erbartos/twitprint/tweettext/tweet.txt print.pbm
+convert -size 600  -pointsize 40 caption:@/home/erbartos/twitprint/tweettext/tweet.txt print.pbm
 
 echo "lets print the image!"
 
 #changing these paramiters doesnt do much, but it makes you feel special
-cat print.pbm | pnmtoplainpnm | pbm2lwxl 900 900 | lp -o raw
+cat print.pbm | pnmtoplainpnm | pbm2lwxl 900 | lp -o raw
 
 echo "DONE"
